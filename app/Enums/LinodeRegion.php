cc5454e47856cbb8e4bd28fecbc23f816c17664d
Shay Punter <icodinqs@gmail.com>
2022-09-25 12:46:20 +0100

    Added linode region

---
+++ app/Enums/LinodeRegion.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static OptionOne()
+ * @method static static OptionTwo()
+ * @method static static OptionThree()
+ */
+enum LinodeRegion: string
+{
+    case AP_WEST = "ap-west";
+    case CA_CENTRAL = "ca-central";
+    case AP_SOUTHWEST = "ap-southwest";
+    case US_CENTRAL = "us-central";
+    case US_WEST = "us-west";
+    case US_SOUTHEAST = "us-southeast";
+    case US_EAST = "us-east";
+    case EU_EAST = "eu-east";
+    case AP_SOUTH = "ap-south";
+    case EU_CENTRAL = "eu-central";
+    case AP_NORTHEAST = "ap-northeast";
+}
