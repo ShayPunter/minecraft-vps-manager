7ef6672ec576196477185dc5f29a8f676f28e6a0
ShayPunter <icodinqs@gmail.com>
2023-12-28 23:37:09 +0100

    fix vue rendering error

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -125,7 +125,7 @@
                     <nav class="flex-1 space-y-1 px-2 pb-4">
                         <a
                             v-for="item in navigation"
-                            v-if="navigation.role === role"
+                            v-if="item.role === role"
                             :key="item.name"
                             :href="item.href"
                             :class="[
